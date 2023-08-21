76f6e840b32aa68127ca645530215969dc5cf8c9
Igor Benić <igbenic@icloud.com>
2023-08-21 10:36:42 +0200

    comment

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import React from "react";
 function App() {
     return (
         <>
+            {/* this should be possible to control+space import, it works when I add ChevronDownRegular to the imports of @fluentui/react-icons manually */}
             <ChevronDownRegular/>
         </>
     );
