0789a538366bc5b854d7b6c7bce727ce76cd237e
Igor Benić <igbenic@icloud.com>
2023-08-21 10:29:19 +0200

    reproduced

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css';
+import {AccessTimeFilled,} from '@fluentui/react-icons';
+import React from "react";
+
+function App() {
+    return (
+        <>
+            <ChevronDownRegular/>
+        </>
+    );
+}
+
+export default App;
